bf16c7c1a8292e881450346d71842f2514f6ce28
Parallels <kayxi2014@gmail.com>
2017-09-14 23:31:03 -0700

    changed the code for notes, can't use C4 because DE1 does not support it. Mi and higher Do still sound a little bit weird

--- lab1/lab1_template_de1soc/Basic_Organ_Solution.cdf
+++ lab1/lab1_template_de1soc/Basic_Organ_Solution.cdf
@@ -1,4 +1,4 @@
-/* Quartus II 64-Bit Version 14.1.0 Build 186 12/03/2014 SJ Full Version */
+/* Quartus II 64-Bit Version 14.1.0 Build 186 12/03/2014 SJ Web Edition */
 JedecChain;
 	FileRevision(JESD32A);
 	DefaultMfr(6E);
@@ -6,7 +6,7 @@ JedecChain;
 	P ActionCode(Ign)
 		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
 	P ActionCode(Cfg)
-		Device PartName(5CSEMA5F31) Path("C:/Users/g5z0b/Documents/CPEN 311/lab1/lab1_template_de1soc/") File("Basic_Organ_Solution.sof") MfrSpec(OpMask(1));
+		Device PartName(5CSEMA5F31) Path("/home/parallels/Documents/CPEN 311/CPEN311_Labs/lab1/lab1_template_de1soc/") File("Basic_Organ_Solution.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
--- lab1/lab1_template_de1soc/SW_CONFIG.sv
+++ lab1/lab1_template_de1soc/SW_CONFIG.sv
@@ -4,15 +4,15 @@ module SW_CONFIG(sw0, sw1, sw2, sw3, out);
 	always@(*)
 	begin
 		case({sw3,sw2,sw1,sw0})
-			4'b0001: out <= 8'hA65A;  //Do 95547 Hex:A65A
-			4'b0011: out <= 8'hBAB5;  //Re 47797 Hex:BAB5
-			4'b0101: out <= 8'h942D;  //Mi 37933 Hex:942D
-			4'b1001: out <= 8'h8BE6;  //Fa 35814 Hex:8BE6
-			4'b0111: out <= 8'h7CB6;  //So 31926 Hex:7CB6
-			4'b1011: out <= 8'h62EF;  //La 25327 Hex:62EF
-			4'b1101: out <= 8'h6EF7;  //Si 28406 Hex:6EF7
-			4'b1111: out <= 8'h5D5B;  //Do 23899 Hex:5D5B
-			default: out <= 8'h0;     //default value
+			4'b0001: out = 8'hBA9E;  //Do 95547 Hex:A65A
+			4'b0011: out = 8'hA65A;  //Re 47797 Hex:BAB5
+			4'b0101: out = 8'h942D;  //Mi 37933 Hex:942D	???
+			4'b0111: out = 8'h8BE6;  //Fa 35814 Hex:8BE6
+			4'b1001: out = 8'h7C8D;  //So 31926 Hex:7CB6
+			4'b1011: out = 8'h62DB;  //La 25327 Hex:62EF
+			4'b1101: out = 8'h6EF7;  //Si 28406 Hex:6EF7
+			4'b1111: out = 8'h5D50;  //Do 23899 Hex:5D5B ???
+			default: out = 8'h0;     //default value
 		endcase
 	end
 endmodule 
