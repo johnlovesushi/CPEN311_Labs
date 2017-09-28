3627ed0721f39946629b873e0830ca712ea73c2f
Ziqiao John Lin <linziqiao0620@gmail.com>
2017-09-27 21:23:33 -0700

    Add files via upload
    used for simulation. The module name has been changed to top

---
+++ lab1/New folder/LED_display.sv
@@ -0,0 +1,29 @@
+module LED_top(clk, LED);
+	input clk;
+	output logic [7:0] LED = 8'b1;
+	logic direction = 0;
+	
+	always @ (posedge clk)
+    begin
+		if (direction == 0)
+		begin
+			if (LED < 8'b1000_0000)
+				LED <= LED << 1;
+			else 
+			begin
+				direction = ~direction;
+				LED <= LED >> 1;
+			end
+		end
+		else
+		begin
+			if (LED > 8'b0000_0001)
+				LED <= LED >> 1;
+			else 
+			begin
+				direction = ~direction;
+				LED <= LED << 1;
+			end
+		end
+	end
+endmodule
---
+++ lab1/New folder/SW_CONFIG.sv
@@ -0,0 +1,19 @@
+module tone_organ_top(sw0, sw1, sw2, sw3, out);
+	input sw0, sw1, sw2, sw3;
+	output logic [31:0] out;
+	
+	always@(*)
+	begin
+		case({sw3,sw2,sw1,sw0})
+			4'b0001: out = 32'hBA9E;  //Do 95547 
+			4'b0011: out = 32'hA65A;  //Re 47797 
+			4'b0101: out = 32'h942D;  //Mi 37933 
+			4'b0111: out = 32'h8BE6;  //Fa 35814 
+			4'b1001: out = 32'h7C8D;  //So 31926 
+			4'b1011: out = 32'h6EF7;  //La 25327 
+			4'b1101: out = 32'h62DB;  //Si 28406 
+			4'b1111: out = 32'h5D50;  //Do 23899
+			default: out = 32'h0;     //default value
+		endcase
+	end
+endmodule 
