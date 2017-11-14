9e283db21b8599cb2f8b0740c8dd0b92f4ad0efc
Ziqiao John Lin <linziqiao0620@gmail.com>
2017-11-13 23:35:06 -0800

    Add files via upload
    FSM still have problem

--- lab4-github/rc4_FSM.sv
+++ lab4-github/rc4_FSM.sv
@@ -13,7 +13,7 @@ logic [7:0]	address;
 logic [7:0]	data;		
 logic			wren;	
 logic [7:0] q;	
-
+logic [6:0] state;  //////////////////////////
 logic	[4:0]	address_d;	//32 words
 logic [7:0] data_d;
 logic 		wren_d;
@@ -26,35 +26,53 @@ logic [23:0] secret_key;	//3 keys, 8-bit each
 logic [7:0] single_sec_key;
 
 logic [1:0] key_num;	//up to 2
-logic [7:0] temp;	//used to swap data
+logic [7:0] data_i;	//used to swap data
+logic [7:0] data_j;
 logic [7:0] sum;
 logic [7:0] f;
 logic [7:0] counter_j;
 logic [7:0] counter_i;
 logic [4:0] counter_k;
-logic [2:0] state;
 
 parameter Key_length = 3;
 
-parameter Idle 		 = 5'b0000_0;
-parameter Loop1_init  = 5'b0001_1;
-parameter Loop1_main  = 5'b0010_1;
-parameter Loop2_init  = 5'b0011_0;
-parameter Loop2_start = 5'b0100_1;
-parameter Loop2_swap1 = 5'b0101_0;
-parameter Loop2_swap2 = 5'b0110_1;
-parameter Loop2_swap3 = 5'b0111_1;
-parameter Finish 		 = 5'b1000_0;
-
+parameter Idle 		 		= 7'b00000_00;
+parameter Loop1_init  = 7'b00001_10;
+parameter Loop1_main  = 7'b00010_10;
+parameter Loop2_init  = 7'b00011_00;
+parameter Loop2_start = 7'b00100_00;
+parameter Loop1_done  = 7'b01001_00;
+parameter Loop2_sum 	 = 7'b00101_00;
+parameter Loop2_read_i = 7'b10001_00;
+parameter Loop2_wait_i = 7'b10010_00;
+parameter Loop2_read_j = 7'b10011_00;
+parameter Loop2_wait_j = 7'b10100_00;
+parameter Loop2_swap_j = 7'b10101_10;
+parameter Loop2_swap_i = 7'b10110_10;
+parameter Loop2_get_j  = 7'b10111_00;
+parameter Loop2_done   = 7'b11000_00;
+parameter Loop2_get_key= 7'b11001_00;
+parameter Finish 		 = 7'b01000_00;
+/*
+parameter Loop3_init  = 7'b01001_00;
+parameter Loop3_start = 7'b01010_00;
+parameter Loop3_inc_j = 7'b01011_00;
+parameter Loop3_swap1 = 7'b01100_00;
+parameter Loop3_swap2 = 7'b01101_00;
+parameter Loop3_calc1 = 7'b01110_00;
+parameter Loop3_calc2 = 7'b01111_00;
+parameter Loop3_calc3 = 7'b10000_00;
+*/
 ksa (CLOCK_50,KEY,SW,LEDR,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
 s_memory sys_memory (address, CLOCK_50, data, wren, q);
 d_message Dec_Msg   (address_d, CLOCK_50, data_d, wren_d, q_d);
-rom		 Enc_Msg	  (address_m, CLOCK_50, 	q_m);
+//rom		 Enc_Msg	  (address_m, CLOCK_50, 	q_m);
 
 //first 14 bits are 0, and the lower 10 are assigned by switches
-assign secret_key[9:0] = 10'b1001001001; //SW[9:0];
+assign secret_key[9:0] = 10'b10_01001001; //SW[9:0];
 assign secret_key[23:10] = 0;
-assign wren = state[0];
+assign wren = state[1];
+assign wren_d = state[0];
 //assign reset = KEY[3];
 always@(posedge CLOCK_50) begin
 		//if (reset)	state <= idle;
@@ -66,33 +84,52 @@ always@(posedge CLOCK_50) begin
 			Loop1_main: 		begin	data <= counter_i;
 											address <= counter_i;
 											counter_i <= counter_i + 1;
-											if ( counter_i = 255 ) state <= Loop2_init;
-											else  state <= Loop1_main; end
+											if ( counter_i == 255 ) state <= Loop1_done;
+											else  state <= Loop1_main; 
+									end
+			Loop1_done: 			state <= Loop2_init;
 			//task 2(a)
 			Loop2_init: 		begin counter_j <= 0;	counter_i <= 0;	state <= Loop2_start; end
-			Loop2_start:		begin 
-											key_num <= counter_i % Key_length;
-											case (key_num)
+			Loop2_start:		begin key_num <= counter_i % Key_length; state <= Loop2_get_key; address <= counter_i; end
+			Loop2_get_key:		begin		case (key_num)
 												0:	single_sec_key <= secret_key[23:16];
 												1: single_sec_key <= secret_key[15:8];
 												2: single_sec_key <= secret_key[7:0];
 												default: single_sec_key <= 0;
-											endcase
-											address <= counter_i;
-											state <= Loop2_swap1; end	//need a cycle to read data from S memory
-			Loop2_swap1:		begin	counter_j <= counter_j + single_sec_key + q;
-											temp <= q;	//temp = s[i];
+												endcase
+												state <= Loop2_read_i; 
+									end	//need a cycle to read data from S memory
+			Loop2_read_i: 		begin address <= counter_i;
+											state <= Loop2_wait_i;
+									end		
+			Loop2_wait_i:  			state <= Loop2_read_j;
+								
+			Loop2_read_j:		begin	data_i <= q;	//data_i = s[i];
+											counter_j <= counter_j + single_sec_key + q;
 											address <= counter_j;	//read s[j]
-											state <= Loop2_swap2; end
-			Loop2_swap2:		begin address <= counter_i; 
-											data <= q;	//wren = 1, s[i] <= s[j];
-											state <= Loop2_swap3; end
-			Loop2_swap3:		begin	address <= counter_j;
-											data <= temp;	//wren = 1, s[j] = temp;
-											counter_i <= counter_i + 1;
-											if (counter_i == 0) state <= Loop3_init;
-											else state <= Loop2_start; end
+											state <= Loop2_wait_j;
+									end
+			Loop2_wait_j:				state <= Loop2_get_j;
+			
+			Loop2_get_j:		begin data_j <= q;
+											state <= Loop2_swap_j;
+									end
+			Loop2_swap_j:		begin data <= data_j;
+        							address <= counter_i; 
+										//wren = 1, s[i] <= s[j];
+											state <= Loop2_swap_i; 
+									end
+			Loop2_swap_i:		begin	data <= data_i;
+        							address <= counter_j;
+                      //wren = 1, s[j] = temp;
+											state <= Loop2_done;
+									end
+			Loop2_done: 		begin counter_i <= counter_i + 1;
+											if (counter_i == 255) state <= Finish;
+											else state <= Loop2_start; 
+									end
 			//task 2(b)
+			/*
 			Loop3_init:			begin counter_i <= 0;
 											counter_j <= 0;
 											counter_k <= 0;
@@ -123,10 +160,10 @@ always@(posedge CLOCK_50) begin
 											if (counter_k == 0) state <= Finish;
 											else state <= Loop3_start; end
 											
+			*/								
 											
-											
-			Finish:						state <= Idle;
+			Finish:	state <= Idle;
 			default: state <= Idle;
 			endcase
 end 
-endmodule
+endmodule	
